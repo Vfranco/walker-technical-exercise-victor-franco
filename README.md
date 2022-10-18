46e69d9f1762b1dda1e47bd8b99519bda1410b68
Victor Daniel Franco Valencia <44242722+vfrancov@users.noreply.github.com>
2022-10-18 13:22:42 -0500

    Walker Assesment

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # WalkerTechVf
 
-Technicall assesment
+Technicall assesment Victor Franco
+
+# Steps
+
+Once the repository has been cloned locate inside the project and execute this command:
+
+```bash
+npm install
+```
+
+Once the node modules has been installed execute this command to start the project
+
+```bash
+npm run start
+```
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'walker-tech-vf';
+
+  inputNumber: number = 0;
+  amountOfNumber: number = 0;
+  isWalker: boolean = false;
+  isAsessement: boolean = false;
+  isWalkerAndAsessement: boolean = false;
+  showError: boolean = false;
+  showNotApply: boolean = false;
+
+  printNumbers(): void {
+    this.amountOfNumber = this.inputNumber;
+    this.checkInput();
+  }
+
+  checkInput() {
+    this.initializeStatus();
+    if (this.amountOfNumber < 1 || this.amountOfNumber > 200)
+      this.showError = true;
+    else {
+      if ((this.amountOfNumber % 5) == 0 && (this.amountOfNumber % 3) == 0) {
+        this.isWalkerAndAsessement = true;
+      } else if ((this.amountOfNumber % 5) == 0) {
+        this.isAsessement = true;
+      } else if ((this.amountOfNumber % 3) == 0) {
+        this.isWalker = true;
+      } else {
+        this.isWalkerAndAsessement = false;
+        this.isAsessement = false;
+        this.isWalker = false;
+        this.showNotApply = true;
+      }
+    }
+  }
+
+  private initializeStatus(): void {
+    this.showError = false;
+    this.isWalkerAndAsessement = false;
+    this.isAsessement = false;
+    this.isWalker = false;
+    this.showNotApply = false;
+  }
 }
